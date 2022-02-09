e16c22334f8c00787f6c3043411c07bcc4c52a8f
Matt Jones <github@jonesm.io>
2022-02-09 10:00:26 +0100

    add polly to handle HTTP 429 status

--- src/Ingestor/Adapters/DependencyResolution.cs
+++ src/Ingestor/Adapters/DependencyResolution.cs
@@ -1,7 +1,10 @@
 ﻿namespace Ingestor.Adapters
 {
     using System;
+    using System.Net.Http;
     using Microsoft.Extensions.DependencyInjection;
+    using Polly;
+    using Polly.Extensions.Http;
     using Ports;
 
     public static class DependencyResolution
@@ -10,14 +13,24 @@
         {
             services.AddTransient<IFetchShowInformation, TvMazeShowProvider>();
 
-            services.AddHttpClient(
-                "tvmaze",
-                configure =>
-                {
-                    configure.BaseAddress = new Uri("https://api.tvmaze.com");
-                });
+            services
+                .AddHttpClient(
+                    "tvmaze",
+                    configure =>
+                    {
+                        configure.BaseAddress = new Uri("https://api.tvmaze.com");
+                    })
+                .AddPolicyHandler(GetRetryPolicy());
 
             return services;
+        }
+
+        static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+        {
+            return HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.TooManyRequests)
+                .WaitAndRetryAsync(6, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
         }
     }
 }
